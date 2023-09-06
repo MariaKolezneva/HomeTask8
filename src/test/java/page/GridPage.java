3a1d1b5719a98cd9fb1996139b075096f0aaf576
MariaKolezneva <77171892+MariaKolezneva@users.noreply.github.com>
2023-09-06 12:34:37 +0300

    Update GridPage.java

--- src/test/java/page/GridPage.java
+++ src/test/java/page/GridPage.java
@@ -6,21 +6,21 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GridPage {
-    public static final SelenideElement GRID_BUTTON = $x("(//div[@class='align-items-center d-flex modes__item-link' and @title='Grid & chart'])");
-    public static final SelenideElement GRID_TABLE_HEADER = $x("//div[@class='app-title flex-1 widget__header']");
-    public static final SelenideElement ORDERS = $x("//div[@class='d-flex flex-column h-100 p-2 w-100 widget']");
-    public static final SelenideElement ORDER_TABLE_HEADER = $x("//div[@class='app-title flex-1 widget__header']");
-    public static final SelenideElement ID_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderId']");
-    public static final SelenideElement TYPE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderType']");
-    public static final SelenideElement INSTRUMENT_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderInstrument']");
-    public static final SelenideElement SIDE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderSide']");
-    public static final SelenideElement START_TIME_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='startTime']");
-    public static final SelenideElement END_TIME_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='endTime']");
-    public static final SelenideElement EXEC_TYPE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderStatus']");
-    public static final SelenideElement EXEC_SIZE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='executedSize']");
-    public static final SelenideElement SIZE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderSize']");
-    public static final SelenideElement SHORTFALL_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='shortFall']");
-    public static final SelenideElement HORIZONTAL_SCROLL = $x("//div[@class='ag-body-horizontal-scroll-viewport']");
+    private static final SelenideElement GRID_BUTTON = $x("(//div[@class='align-items-center d-flex modes__item-link' and @title='Grid & chart'])");
+    private static final SelenideElement GRID_TABLE_HEADER = $x("//div[@class='app-title flex-1 widget__header']");
+    private static final SelenideElement ORDERS = $x("//div[@class='d-flex flex-column h-100 p-2 w-100 widget']");
+    private static final SelenideElement ORDER_TABLE_HEADER = $x("//div[@class='app-title flex-1 widget__header']");
+    private static final SelenideElement ID_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderId']");
+    private static final SelenideElement TYPE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderType']");
+    private static final SelenideElement INSTRUMENT_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderInstrument']");
+    private static final SelenideElement SIDE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderSide']");
+    private static final SelenideElement START_TIME_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='startTime']");
+    private static final SelenideElement END_TIME_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='endTime']");
+    private static final SelenideElement EXEC_TYPE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderStatus']");
+    private static final SelenideElement EXEC_SIZE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='executedSize']");
+    private static final SelenideElement SIZE_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='orderSize']");
+    private static final SelenideElement SHORTFALL_COLUMN = $x("//div[@class='ag-header-cell ag-header-cell-sortable' and @col-id='shortFall']");
+    private static final SelenideElement HORIZONTAL_SCROLL = $x("//div[@class='ag-body-horizontal-scroll-viewport']");
 
     public static void openGridPage() {
         GRID_BUTTON.shouldBe(visible);
