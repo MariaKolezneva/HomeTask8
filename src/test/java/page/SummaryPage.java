2b90e81dd6d7a7fa4c7f6ef7e4c724e6ae5a05d7
MariaKolezneva <77171892+MariaKolezneva@users.noreply.github.com>
2023-09-06 12:36:43 +0300

    Update SummaryPage.java

--- src/test/java/page/SummaryPage.java
+++ src/test/java/page/SummaryPage.java
@@ -7,8 +7,8 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class SummaryPage {
-    public static final SelenideElement ALGO_PERFORMANCE = $x("//div[@class='app-title flex-1 widget__header' and text()='Algo Performance']");
-    public static final SelenideElement WIDGET = $x("(//div[@class='widget-icon' and @title='Expand'])[2]");
+    private static final SelenideElement ALGO_PERFORMANCE = $x("//div[@class='app-title flex-1 widget__header' and text()='Algo Performance']");
+    private static final SelenideElement WIDGET = $x("(//div[@class='widget-icon' and @title='Expand'])[2]");
     private static final List<SelenideElement> HEADER_TITLES = $$(By.xpath("//div[contains(@class, 'simple-table__row simple-table__row_grid-template-columns-4')]//div[contains(@class, 'text-right simple-table__header-title')]"));
     private static final List<SelenideElement> LINE_NAMES = $$(By.xpath("//div[contains(@class, 'simple-table__grid')]//div[contains(@class, 'simple-table__row simple-table__row_grid-template-columns')]//div[contains(@class, 'pl-1 simple-table__row-value')]"));
     private static final List<SelenideElement> STRING_VALUES = $$(By.xpath("//div[contains(@class, 'simple-table__grid')]//div[contains(@class, 'simple-table__subgrid')]//div[contains(@class, 'text-right simple-table__row-value')]"));
